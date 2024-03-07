1b13d5add475125f38bdd7b2fb1b4e65b6623e56
GuillaumePons63 <contact@guillaumepons.fr>
2024-03-07 16:32:06 +0100

    prépa seed

--- guillaumeWeb/seeders/20231018103809-demo-activities.cjs
+++ guillaumeWeb/seeders/20231018103809-demo-activities.cjs
@@ -1,8 +1,19 @@
 'use strict';
 
+
+
+
+
+
+
 /** @type {import('sequelize-cli').Migration} */
+
+
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
+
+
+  async up(queryInterface) {
     /**
      * Add seed commands here.
      *
@@ -11,46 +22,51 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
+    
     */
-    return queryInterface.bulkInsert('activities', [
-      {
-        title: 'Activité 1',
-        description: 'Description pour l\'activité 1',
-        image: null,
-        link: null,
-        mainContent: 'Contenu principal de l\'activité 1',
-        date: JSON.stringify([new Date(), new Date()]),
-        enCours: false,
-        technology: '[React,NodeJs]',
+    const data = require('./data.cjs')
+
+    return queryInterface.bulkInsert('activities',
+      data, {})
+    // [
+    //   {
+    //     title: 'Activité 1',
+    //     description: 'Description pour l\'activité 1',
+    //     image: null,
+    //     link: null,
+    //     mainContent: 'Contenu principal de l\'activité 1',
+    //     date: JSON.stringify([new Date(), new Date()]),
+    //     enCours: false,
+    //     technology: '[React,NodeJs]',
 
 
-      },
-      {
-        title: 'Activité 2',
-        description: 'Description pour l\'activité 2',
-        image: null,
-        link: null,
-        mainContent: 'Contenu principal de l\'activité 2',
-        date: JSON.stringify([new Date(), new Date(), new Date()]),
-        enCours: true,
-        technology: '[React,NodeJs]',
-      },
-      {
-        title: 'Activité 3',
-        description: 'Description pour l\'activité 3',
-        image: null,
-        link: null,
-        mainContent: 'Contenu principal de l\'activité 3',
-        date: JSON.stringify([new Date(), new Date(), new Date(), new Date()]),
-        enCours: false,
-        technology: '[React,NodeJs]',
-      }
+    //   },
+    //   {
+    //     title: 'Activité 2',
+    //     description: 'Description pour l\'activité 2',
+    //     image: null,
+    //     link: null,
+    //     mainContent: 'Contenu principal de l\'activité 2',
+    //     date: JSON.stringify([new Date(), new Date(), new Date()]),
+    //     enCours: true,
+    //     technology: '[React,NodeJs]',
+    //   },
+    //   {
+    //     title: 'Activité 3',
+    //     description: 'Description pour l\'activité 3',
+    //     image: null,
+    //     link: null,
+    //     mainContent: 'Contenu principal de l\'activité 3',
+    //     date: JSON.stringify([new Date(), new Date(), new Date(), new Date()]),
+    //     enCours: false,
+    //     technology: '[React,NodeJs]',
+    //   }
 
-      // ... Ajoutez autant d'entrées que vous le souhaitez
-    ]);
+    // ... Ajoutez autant d'entrées que vous le souhaitez
+    // ]);
   },
 
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     /**
      * Add commands to revert seed here.
      *
---
+++ guillaumeWeb/seeders/data.cjs
@@ -0,0 +1,26 @@
+
+const data = [
+    {
+        title: 'Activité 1',
+        description: 'Description pour l\'activité 1',
+        image: null,
+        link: null,
+        mainContent: 'Contenu principal de l\'activité 1',
+        date: JSON.stringify([new Date(), new Date()]),
+        enCours: false,
+        technology: '[React,NodeJs]'
+    },
+    {
+
+        title: 'Formateur en informatique à Montluçon',
+        description: 'Grâce à l\'opportunité offerte par la CCI de l\'Allier, j\'ai encadré deux groupes dans le cadre de formations PICs numériques divergentes, les initiant au vaste univers du développement informatique et de la création de sites web, avec une mention spéciale pour un groupe qui a brillamment décroché le TOSA WordPress.',
+        image: "./imagesProjet/cci-allier-web.jpg",
+        link: "https://www.allier.cci.fr/",
+        mainContent: "<main> <section> <h2>Introduction</h2><p>Grâce à l'opportunité offerte par la <strong>CCI de l'Allier</strong>, j'ai eu le privilège d'encadrer deux groupes dans le cadre de formations PICs numériques, leur ouvrant les portes du développement informatique et de la création de sites web.Une expérience particulièrement marquée par la réussite d'un groupe à l'examen TOSA WordPress.</p></section><section><h2>Mon Défi</h2><p>Adapter les sessions pour répondre aux besoins spécifiques de chaque groupe tout en maintenant un haut niveau d'engagement et de motivation était mon principal défi. L'objectif était de transformer chaque participant en un acteur compétent du monde numérique.</p></section><section><h2>Le Programme</h2><p>Le programme, ambitieux et diversifié, couvrait les fondamentaux de la programmation, la conception web, et l'utilisation efficace de CMS, notamment WordPress. Un moment fort a été la préparation et la réussite du groupe à l'examen TOSA WordPress, soulignant leur maîtrise de cet outil essentiel.</p></section><section><h2>Impact et Réalisation</h2><p>La réussite à l'examen TOSA WordPress fut un témoignage vibrant de l'efficacité de notre approche pédagogique. Pour moi, cela a renforcé l'importance de l'adaptabilité et de l'engagement dans l'enseignement du numérique.</p></section><section><h2>Conclusion</h2><p>Cette expérience a été une étape significative, enrichissant ma carrière et renforçant mon désir de contribuer à l'écosystème numérique. Je suis reconnaissant pour cette opportunité avec la CCI de l'Allier, et je suis excité à l'idée de poursuivre cette aventure éducative.</p></section></main>",
+        date: null,
+        enCours: false,
+        technology: "[HTML, CSS, JavaScript, WordPress, TOSA]"
+    }
+]
+
+module.exports = data;
