fc78017dd24012cd78942ec44ecfc1e1a66223fd
GuillaumePons63 <guillaume@web-63.fr>
2025-09-21 11:39:09 +0200

    ajout studio

--- guillaumeWeb/nuxt.config.ts
+++ guillaumeWeb/nuxt.config.ts
@@ -15,9 +15,38 @@ export default defineNuxtConfig({
         pass: process.env.MAIL_PASSWORD
       }
     }
-  }], "@nuxt/content", "@nuxt/image"],
+  }], "@nuxt/content", "@nuxt/image",'@nuxt/content'],
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio',
+      // Utile sur hébergement "custom" (o2switch) pour aider Studio à te reconnaître :
+      gitInfo: {
+        name: 'GuillaumeWeb',           // ex: 'site-vitrine'
+        owner: 'GuillaumePons63',    // ex: 'guillaumeweb'
+        url: 'https://github.com/GuillaumePons63/GuillaumeWeb'
+      }
+    }
+  },
   devtools: { enabled: true },
-  css: ['~/assets/css/custom.scss'],
+  css:  [
+    '@/assets/css/custom.scss',
+    'animate.css/animate.min.css', // en tant que CSS, pas SCSS
+    '@/assets/css/animation.css'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+           silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        }
+      }
+    }
+  },
   nitro: {
     preset: 'node-server'
   },
